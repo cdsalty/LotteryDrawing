afbad1e237c2bcd0c77e415e4f0e50aaaa80eb4f
cdsalty <soltiscd@gmail.com>
2019-05-20 16:17:50 -0400

    very small update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Ball from './Ball';
+// import Ball from './Ball';
 import Lottery from './Lottery';
 import './App.css';
 
--- src/Lottery.js
+++ src/Lottery.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react';
 import Ball from './Ball';
 
+/*
+Logic Ahead
+
+Props:
+    - title: title of the lottory game
+    - numBalls: number of balls to display
+    - maxNumber: maximum value of a ball
+State: 
+    - nums: array of [num, num, num, ...] for balls
+Events: 
+    - onClick: to regenerates the nums in state (a new set of balls/values)
+*/
 
 class Lottery extends Component {
     static defaultProps = {
@@ -10,12 +22,17 @@ class Lottery extends Component {
     }
     constructor(props){
         super(props);
-        // this.state = { nums: [] }; instead of making into an empty array, instead...
-        this.state = { nums: Array.from({ length: this.props.maxBalls }) }; // length: array from 0 up to maxBalls
+        // this.state = { nums: [] }; maxBalls controls the state
+        this.state = { nums: Array.from({ length: this.props.maxBalls }) }; 
+            // length: is equal to an array from 0 up to maxBalls
     }
     
     generateNumbers(){
-        //Pick numbers from 1 to maxNumber and how many times is based on how many maxBalls is set to.
+        // Pick a random ranging from 1 to maxNumber.
+        // maxBalls will determine how may balls/numbers to render.
+            // create an array and then add them to this.state under 'nums:'
+        // loop over, map over nums in state, each one build a random new number and set it state.
+        
         
 
     }
@@ -31,7 +48,7 @@ class Lottery extends Component {
                   {this.state.nums.map(n => (
                   <Ball num = {n} />
                   ))}
-                  {/* for our number, map over each number and return a Ball component with a num = n */}
+                {/* for our number, map over each number and return a Ball component with each number = n */}
               </div>
               <button onClick = {this.handleClick}>Click to Play</button>
           </section>
