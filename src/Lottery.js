b16bfcad947b1ac5077ff1e6ac57cbd46c8431e6
cdsalty <soltiscd@gmail.com>
2019-05-21 16:00:53 -0400

    numbers now randomly generate on click

--- src/Lottery.js
+++ src/Lottery.js
@@ -22,23 +22,27 @@ class Lottery extends Component {
     }
     constructor(props){
         super(props);
-        // this.state = { nums: [] }; maxBalls controls the state
         this.state = { nums: Array.from({ length: this.props.maxBalls }) }; 
             // length: is equal to an array from 0 up to maxBalls
+            // this.state = { nums: [] }; maxBalls controls the state
+        this.handleClick = this.handleClick.bind(this);
     }
     
     generateNumbers(){
-        // Pick a random ranging from 1 to maxNumber.
+        // needs to pick a random number between 1 and maxNumber
         // maxBalls will determine how may balls/numbers to render.
             // create an array and then add them to this.state under 'nums:'
-        // loop over, map over nums in state, each one build a random new number and set it state.
-        
-        
-
+        // take the logic from getting a random number, place it inside .map() and assign to this.setState.
+        // MUST REMEMBER TO USE THIS.PROPS.MAXNUMBER
+        this.setState(currentState => ({
+          nums: currentState.nums.map(
+              n => Math.ceil(Math.random() * this.props.maxNumber)
+          )
+        }));
     }
 
     handleClick() {
-        this.generateNumbers(); // MUST INVOKE () &NOT this.generateNumbers and no paranthesis. 
+        this.generateNumbers(); // MUST INVOKE () & NOT this.generateNumbers and no paranthesis. 
     }
     render() {
         return(
